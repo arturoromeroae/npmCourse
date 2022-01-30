f2f0c2de576f06c98a618c9b5e790c615874a1e2
Arturo Romero <aromero@aeonlinesolutions.com>
2022-01-30 01:01:19 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const messages = [
+    "Andres",
+    "Pedro",
+    "Jessica",
+    "Diego",
+    "Laura"
+];
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+}
+
+module.exports = { randomMsg };
